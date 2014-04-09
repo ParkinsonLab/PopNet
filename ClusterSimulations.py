a8c8929a50f9f2fba36feebb14e3e891bd87f2d6
xescape <javi_zhang@hotmail.com>
2014-04-09 14:57:14 -0400

    Complete cluster simulations
    did about half of it.
    
    [ticket: X]
    
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   .pydevproject
    	new file:   .settings/org.eclipse.ltk.core.refactoring.prefs
    	modified:   ClusterSimulations.py

--- ClusterSimulations.py
+++ ClusterSimulations.py
@@ -7,8 +7,43 @@ Created on Apr 7, 2014
 This category is started with comparing the clustering patterns of random strain-pairs.
 However, other similar functins may evolve from this, and should be included in this script.'''
 
-def strainPairComparison():
+
+'''(clusterTree, list of names(currently expects 2) -> Tree of numbers, arranged by chromosome
+This function determines whether all the included strains cluster together at every position. 
+It iterates through all clusters, compares, and gives out either 1 or 0 as a result. 
+Output: data for each chromosome is transformed to a list of 1s and 0s. 
+Format: {chr:[0, 1...]}'''
+def strainPairComparison(clusters, strains):
+    for chr in clusters:
+
+'''helper function for strainPairComparison. 
+determines if the strains are identical in this block'''
+def identical(clusters, strains):        
+    for line in clusters:
+        if set(strains).issubset(set(line)):
+            return True
+    
+'''(List) -> list of tuples representing blocks
+iterates through a list using a moving window, and deterine whether a particular
+section is considered identical or not. Requires at least 5 consecutive identities
+to begin an identity block. '''
+def prune(numbers):
     pass
+
+'''(matrix) -> dictionary of key sites: relevant strains
+gives a NxN matrix containing identity information between strains, find the important
+blocks. Input should be from prune(). '''
+def findKeyBlocks(identityMatrix):
+    pass
+
+'''(clusterTree) -> Dict of key sites: relevant strains
+This is the main function for the simulation. Interesting 
+sites where many strains share identity will be picked out and
+listed, along with the relevant strains.'''
+def simulation(clusters):
+    pass
+
+
 
 
 if __name__ == '__main__':
